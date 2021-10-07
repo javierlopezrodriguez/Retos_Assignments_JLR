0ad6544568124c89de3239a8565e178d983d4c40
Javier Lopez Rodriguez <javierlopezbq@gmail.com>
2021-10-07 07:14:50 -0400

    fabricator

--- Assignment1/GeneObject.rb
+++ Assignment1/GeneObject.rb
@@ -1,32 +1,45 @@
 class Gene
   
-    attr_accessor :name
-    attr_accessor :mutant_phenotype
+    #Attributes:
+      #@id:
+      #@name:
+      #@mutant_phenotype:
+      #@linked:
     
-    def initialize (params = {}) # hash with parameters for initialization
-        @name = params.fetch(    ) # COMPLETE
-        @mutant_phenotype = params.fetch(    ) # COMPLETE
-        
-        input_id = params.fetch(     ) # COMPLETE
-        # Checking that the ID has a correct format
-        id_regexp = Regexp.new(/A[Tt]\d[Gg]\d\d\d\d\d/) # creating the regexp for the ID
-        if id_regexp.match(input_id) # if the format is correct (the id matches the regexp)
-            @id = input_id
-        else # if the format is incorrect
-            
+    # Attribute accessors for @name, @mutant_phenotype and @linked
+    attr_accessor :name, :mutant_phenotype, :linked
+    # Attribute reader only for @id
+    attr_reader :id # only want to get an @id, not to change it.
+    # The only way to set an @id should be when creating an instance of Gene.
+    
+    # For initializing a Gene object, I'm using the factory method adapted from
+    # https://stackoverflow.com/questions/15499613/ruby-cancel-object-creation
+    # because I want to create the object only if the @id is formatted correctly
+    # and this way seems cleaner to me.
+    
+    # Basic initializer
+    # (we won't use it directly, to create an object we will call Gene.fabricate)
+    def initialize (params)
+        @id = params.fetch(:id) # no default because this only gets called if it is correct
+        @name = params.fetch(:name, "unknown_name")
+        @mutant_phenotype = params.fetch(:mutant_phenotype, "unknown_mutant_phenotype")
+        @linked = nil # default value at the start, when no hybrid cross has been made
+    end
+    
+    # Fabricator
+    # This is the method that will be called to create an object
+    # It checks if the id is correct:
+    #   if it isn't, it prints a message and returns nil, cancelling the creation.
+    #   if it is, it calls the initializer
+    def self.fabricate (params = {})
+        input_id = params.fetch(:id, "No ID provided")
+        unless Regexp.new(/A[Tt]\d[Gg]\d\d\d\d\d/).match(input_id) # if the id format doesn't match the regexp
+            puts "The entry with ID: #{input_id} doesn't have a correct ID. It is being ignored."
+            return nil # returns nil instead of creating an instance of Gene.
+        else
+            new(params) # if the id is correct, it calls initialize, creating the instance of Gene.
         end
-        
-        
-    if mesh_regex.match(newvalue)
-      @mesh = newvalue
     end
-  end
-  
-  def initialize (params = {})
-    @name = params.fetch(:name, 'unknown disease')
-    @mesh = params.fetch(:mesh, "0000000") # this is the default, i'm not validating this one
-  end
-        
-    end
+    
     
 end
