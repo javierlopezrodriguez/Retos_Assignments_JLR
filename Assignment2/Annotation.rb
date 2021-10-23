8c3f2518a183cc801e0a299d0a6de9f3429f69bd
Javier Lopez Rodriguez <javierlopezbq@gmail.com>
2021-10-23 17:45:08 -0400

    go, kegg annotations, and reworked annotation.rb to also be able to use hashes, and added/reworked tests in annotationtest

--- Assignment2/Annotation.rb
+++ Assignment2/Annotation.rb
@@ -1,35 +1,133 @@
+require 'rest-client'
+require 'json'
+
 class Annotation
 
     # General use Annotation object
 
     # Attributes:
-    # @annotations_hash: a hash that, for each annotation type (key), contains an array of arrays
-    # (there could be multiple annotations of the same type)
-    # example: @annotations_hash["GO"] could contain [["GO:0005634", "C:nucleus", "IEA:UniProtKB-SubCell"], ["GO:0000151", "C:ubiquitin ligase complex", "IBA:GO_Central"]]
+    # @annotations_hash: a hash that, for each annotation type (key), contains an array or a hash (depending on the annotation type)
+    # example if an annotation type contains an array: (in this case an array of arrays)
+    # @annotations_hash["GO"] could contain [["GO:0005634", "C:nucleus", "IEA:UniProtKB-SubCell"], ["GO:0000151", "C:ubiquitin ligase complex", "IBA:GO_Central"]]
+    # same example if an annotation type contains a hash:
+    # @annotations_hash["GO"] could contain {"GO:000534" => ["C:nucleus", "IEA:UniProtKB-SubCell"], "GO:0000151" => ["C:ubiquitin ligase complex", "IBA:GO_Central"]}
     
-    # Only adding a reader so that the annotations get added via the functions defined below
-    attr_reader :annotations_hash
+    attr_accessor :annotations_hash
 
     # on initialization the hash is empty
     def initialize
         @annotations_hash = {}
     end
 
-    # Function to add one annotation
-    def add_one_annotation(key, values)
-        unless @annotations_hash.key?(key)
-            @annotations_hash[key] = [] # empty array if there isn't any with that key
+    # Function to add one annotation as an array or an array of arrays
+    def add_one_annotation_as_array(key, value)
+        @annotations_hash[key] = [] unless @annotations_hash.key?(key) # empty array if there isn't any with that key
+        if @annotations_hash[key].is_a?(Array)
+            @annotations_hash[key] << value unless @annotations_hash[key].include?(value)  # appending value to the empty array if it doesn't exist already
+        else # @annotations_hash[key] isn't an Array (it existed previously and contains a different data structure)
+            puts "WARNING: the annotation of type #{key} is not an Array"
         end
-        @annotations_hash[key] << values # appending values to the empty array
-        # if values is only one element, we will have [a, b, c, ...]
-        # if values is an array, we will have [[a1, a2], [b1, b2], ...]
+        # if value is only one element, we will have [a, b, c, ...]
+        # if value is an array, we will have [[a1, a2], [b1, b2], ...]
     end
 
-    # Function to add multiple annotations
-    def add_multiple_annotations(key, array_of_values)
-        # for each values (array or single element) in array_of_values
-        array_of_values.each do |values|
-            add_one_annotation(key, values)
+    # Function to add one annotation as a hash
+    def add_one_annotation_as_hash(key, input_hash)
+        @annotations_hash[key] = {} unless @annotations_hash.key?(key) # empty hash if there isn't anything with that key
+        input_hash.each do |id, values|
+            # if @annotations_hash[key] isn't a Hash (it existed previously and contains a different data structure):
+            puts "WARNING: the annotation of type #{key} is not a Hash" unless @annotations_hash[key].is_a?(Hash)
+            # if @annotations_hash[key] is a Hash, store values directly if @annotations_hash[key][id] doesn't exist:
+            @annotations_hash[key][id] = values if @annotations_hash[key].is_a?(Hash) && !@annotations_hash[key].key?(id)
+            # if @annotations_hash[key] is a Hash, @annotations_hash[key][id] already exists, and the values are different, give a warning:
+            if @annotations_hash[key].is_a?(Hash) && @annotations_hash[key].key?(id) && @annotations_hash[key][id] != values
+                puts "WARNING: in the annotation of type #{key}, the entry #{id} already contained #{@annotations_hash[key][id].to_s}."
+                puts "#{values.to_s} is not being added."
+            end
         end
     end
+
+    '''
+Annotate it with any KEGG Pathways the interaction network members are part of
+
+both KEGG ID and Pathway Name
+
+Annotate it with the GO Terms associated with the total of all genes in the network
+
+BUT ONLY FROM THE biological_process part of the GO Ontology!
+
+Both GO:ID and GO Term Name
+    '''
+
+    # Function fetch to access an URL via code, donated by Mark Wilkinson.
+    def fetch(url, headers = {accept: "*/*"}, user = "", pass="")
+        response = RestClient::Request.execute({
+            method: :get,
+            url: url.to_s,
+            user: user,
+            password: pass,
+            headers: headers})
+        return response
+        
+        rescue RestClient::ExceptionWithResponse => e
+            $stderr.puts e.inspect
+            response = false
+            return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+        rescue RestClient::Exception => e
+            $stderr.puts e.inspect
+            response = false
+            return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+        rescue Exception => e # generic
+            $stderr.puts e.inspect
+            response = false
+            return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+    end 
+
+    def get_kegg_annotation()
+        # makes the array into an array in case there is only one element
+        id_list = [id_list] unless id_list.respond_to? :each
+        # for each id in the array
+        id_list.each do |id|
+            # this URL works with AGI locus codes
+            url = "http://togows.org/entry/kegg-genes/ath:#{id}/pathways.json" # id can be a symbol, there is implicit conversion to string
+            response = fetch(url)
+            if response # if fetch was successful
+                body = JSON.parse(response.body) # turns the JSON file into a ruby data structure made of arrays and hashes
+                # for each {kegg_id: kegg_pathway_name} hash in body
+                body.each do |kegg_hash|
+                    # for each hash
+                    kegg_hash.each do |kegg_path_id, kegg_path_name|
+                        add_one_annotation_as_hash(:KEGG, {kegg_path_id => kegg_path_name})
+                    end
+                end
+            end
+        end
+    end
+
+    def get_go_annotation(id_list)
+        # makes the array into an array in case there is only one element
+        id_list = [id_list] unless id_list.respond_to? :each
+        # for each id in the array
+        id_list.each do |id|
+            # this URL works with AGI locus codes
+            url = "http://togows.org/entry/ebi-uniprot/#{id}/dr.json" # id can be a symbol, there is implicit conversion to string
+            response = fetch(url)
+            if response # if fetch was successful
+                body = JSON.parse(response.body) # turns the JSON file into a ruby data structure made of arrays and hashes
+                body.each do |hash|
+                    hash["GO"].each do |go_info|
+                        # go_info is an array, for instance: ["GO:0080671", "P:phosphatidylglycerol metabolic process", "IMP:TAIR"]
+                        # getting only the biological processes, whose second field (go_info[1]) starts with 'P:'
+                        matchobj = go_info[1].match(/P:(.*)/)
+                        if matchobj
+                            go_id, go_process = go_info[0], matchobj[1]
+                            # adding the annotation in the format: {"GO:0080671" => "phosphatidylglycerol metabolic process"}
+                            add_one_annotation_as_hash(:GO, {go_id => go_process})
+                        end
+                    end
+                end
+            end
+        end
+    end
+
 end
--- Assignment2/AnnotationTest.rb
+++ Assignment2/AnnotationTest.rb
@@ -5,39 +5,107 @@ require_relative './Annotation.rb' # require_relative so that the path is relati
 
 class AnnotationTest < Minitest::Test
   
-    def test_annotate_single_value
+    def test_annotate_array_single_value
         ann = Annotation.new
-        ann.add_one_annotation("A", "test")
+        ann.add_one_annotation_as_array("A", "test")
         ann_hash = ann.annotations_hash
         #puts ann_hash
 
         assert ann_hash.key?("A") && ann_hash["A"].is_a?(Array) && ann_hash["A"][0] == "test" && !ann_hash["A"][0].is_a?(Array)
     end
 
-    def test_annotate_single_array
+    def test_annotate_array_single_array
         ann = Annotation.new
-        ann.add_one_annotation("A", ["test1", "test2"])
+        ann.add_one_annotation_as_array("A", ["test1", "test2"])
         ann_hash = ann.annotations_hash
         #puts ann_hash
 
         assert ann_hash.key?("A") && ann_hash["A"].is_a?(Array) && ann_hash["A"][0].is_a?(Array) && ann_hash["A"][0][0] == "test1"
     end
 
-    def test_annotate_multiple_values
+    def test_annotate_array_multiple_values
         ann = Annotation.new
-        ann.add_multiple_annotations("A", ["ann1", "ann2", "ann3"])
+        ["ann1", "ann2", "ann3"].each {|ann_value| ann.add_one_annotation_as_array("A", ann_value)}
         ann_hash = ann.annotations_hash
         #puts ann_hash
 
         assert ann_hash.key?("A") && ann_hash["A"].is_a?(Array) && ann_hash["A"][2] == "ann3" && !ann_hash["A"][0].is_a?(Array)
     end
 
-    def test_annotate_multiple_arrays
+    def test_annotate_array_multiple_arrays
         ann = Annotation.new
-        ann.add_multiple_annotations("A", [["ann1_first", "ann1_second"], ["ann2_first", "ann2_second"]])
+        [["ann1_first", "ann1_second"], ["ann2_first", "ann2_second"]].each {|ann_array| ann.add_one_annotation_as_array("A", ann_array)}
         ann_hash = ann.annotations_hash
         #puts ann_hash
 
         assert ann_hash.key?("A") && ann_hash["A"].is_a?(Array) && ann_hash["A"][0].is_a?(Array) 
+    end
+
+    def test_annotate_array_repeated_multiple_arrays
+        ann = Annotation.new
+        [["A", "B"], ["A", "A"], ["B", "B"], ["A"], ["B"], ["A"], ["B"], ["A", "B", "B"], ["A", "B", "C"], ["A", "B"]].each do |ann_array|
+            ann.add_one_annotation_as_array(:A, ann_array)
+        end
+        ann_hash = ann.annotations_hash
+        # should be: {A: [["A", "B"], ["A", "A"], ["B", "B"], ["A"], ["B"], ["A", "B", "B"], ["A", "B", "C"]]}
+        ann_array = ann_hash[:A]
+        #puts ann_array.to_s
+        assert ann_array.length == 7 && ann_array[3] == ["A"] && ann_array[4] == ["B"] && ann_array[5] == ["A", "B", "B"] && ann_array[6] == ["A", "B", "C"]
+    end
+
+    def test_annotate_array_when_already_has_hash
+        ann = Annotation.new
+        ann.annotations_hash["A"] = {} # empty hash
+        ann.add_one_annotation_as_array("A", ["B1", "B2"]) # should print a warning and not do anything
+        ann_hash = ann.annotations_hash
+        
+        # Checking that it is still a hash and hasn't introduced anything
+        assert ann_hash["A"].is_a?(Hash) && !ann_hash["A"].is_a?(Array) && ann_hash["A"].empty?
+    end
+
+    def test_annotate_hash_when_already_has_array
+        ann = Annotation.new
+        ann.annotations_hash["A"] = [] # empty array
+        ann.add_one_annotation_as_hash("A", {B1: "B2"}) # should print a warning and not do anything
+        ann_hash = ann.annotations_hash
+
+        # Checking that it is still an array and hasn't introduced anything
+        assert !ann_hash["A"].is_a?(Hash) && ann_hash["A"].is_a?(Array) && ann_hash["A"].empty?
+    end
+
+    def test_annotate_hash
+        ann = Annotation.new
+        ann.add_one_annotation_as_hash("A", {bkey: :bvalue})
+        ann_hash = ann.annotations_hash
+        #puts ann_hash
+
+        assert ann_hash.key?("A") && ann_hash["A"].key?(:bkey) && ann_hash["A"].keys.length == 1 && ann_hash["A"][:bkey] == :bvalue
+    end
+
+    def test_annotate_hash_diff_key
+        ann = Annotation.new
+        ann.add_one_annotation_as_hash("A", {bkey: :bvalue})
+        ann.add_one_annotation_as_hash("A", {otherkey: :othervalue})
+        ann_hash = ann.annotations_hash
+
+        assert ann_hash.key?("A") && ann_hash["A"].key?(:bkey) && ann_hash["A"].key?(:otherkey) && ann_hash["A"].keys.length == 2 && ann_hash["A"][:bkey] == :bvalue && ann_hash["A"][:otherkey] == :othervalue
+    end
+
+    def test_annotate_hash_multiple_same_key_same_value
+        ann = Annotation.new
+        ann.add_one_annotation_as_hash("A", {bkey: :bvalue})
+        ann.add_one_annotation_as_hash("A", {bkey: :bvalue}) # should not do anything because it is already there
+        ann_hash = ann.annotations_hash
+
+        assert ann_hash.key?("A") && ann_hash["A"].key?(:bkey) && ann_hash["A"].keys.length == 1 && ann_hash["A"][:bkey] == :bvalue && ann_hash["A"].values.length == 1
+    end
+
+    def test_annotate_hash_multiple_same_key_diff_value
+        ann = Annotation.new
+        ann.add_one_annotation_as_hash("A", {bkey: :bvalue})
+        ann.add_one_annotation_as_hash("A", {bkey: :different_value}) # # should print a warning and not do anything
+        ann_hash = ann.annotations_hash
+
+        assert ann_hash.key?("A") && ann_hash["A"].key?(:bkey) && ann_hash["A"].keys.length == 1 && ann_hash["A"][:bkey] == :bvalue && ann_hash["A"].values.length == 1
     end
 end
