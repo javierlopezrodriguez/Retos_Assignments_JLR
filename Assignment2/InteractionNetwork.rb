680eef963d0b536d533f481173538987cdbeea67
Javier Lopez Rodriguez <javierlopezbq@gmail.com>
2021-10-25 18:14:06 -0400

    not really advancing on create_networks

--- Assignment2/InteractionNetwork.rb
+++ Assignment2/InteractionNetwork.rb
@@ -1,4 +1,5 @@
 require_relative './Annotation.rb' # require_relative so that the path is relative to this file
+require 'rgl/'
 
 class InteractionNetwork < Annotation
 
@@ -93,13 +94,14 @@ class InteractionNetwork < Annotation
                     next if gene1 == gene2 # if both are the same gene, skip
                     new_gene = gene1 if gene2 == gene_id # if the original gene_id used in the search is gene2, the new gene is gene1
                     new_gene = gene2 if gene1 == gene_id # and viceversa
+                    next unless gene2 == gene_id || gene1 == gene_id # shouldn't happen but just in case none of the genes equal the original gene_id, skip it
                     InteractionNetwork.add_interaction(gene_id, new_gene) # adding the interaction
                 end
             end
         end
     end
 
-    def self.create_networks()
+    #def self.create_networks()
         # for each gene in the gene list:
             # get all the interactions
             # mark that gene as completed or something
@@ -123,7 +125,54 @@ class InteractionNetwork < Annotation
         # MIRA STRUCTS PARA NO ESTAR REBUSCANDO MIL VECES SI TAL SYMBOL ESTA EN LA GENELIST O NO!! en plan structs para usar aqui simplemente, cuando haces la lista de los genes etc. i guess
         # a lo mejor no tiene sentido eh 
 
-    end
+    #end
+
+=begin     def self.create_networks()
+        # creating a hash to store the genes as I go through them
+        total_genes = {}
+        @@gene_array.each do |gene|
+
+        end
+        # for each gene in @@gene_array (from our .txt gene list)
+        @@gene_array.each do |gene1|
+            successful? = false # boolean value, it is true when the network contains two or more genes that come from @@gene_array
+            # hash to store the genes of the current network, their interactions, and if they're part of @@gene_array or not
+            network_genes = {}
+            # updating the entry at the hash, it has been visited (now) and is part of @@gene_array
+            total_genes[gene1] = {visited?: true, in_gene_list?: true}
+            # getting its interactions
+            gene1_inter = @@all_interactions[gene1]
+            # if there are interactions (not nil)
+            if gene1_inter
+                # adding the gene to the network_genes hash
+                network_genes[gene1] = {in_gene_list?: true, interactions: []} # gene1 comes from @@gene_array
+                # for each interaction
+                gene1_inter.each do |gene2|
+                    # if gene2 is in the hash already (not nil)
+                    if total_genes[gene2]
+                        next if total_genes[gene2][:visited?] # next if gene2 has been visited already
+                        # if it exists in the hash but hasn't been visited yet
+                        total_genes[gene2][:visited?] = true # setting it as visited
+                        in_gene_list = total_genes[gene2][:in_gene_list?] # true if it is part of @@gene_array
+                        successful? = successful? || in_gene_list # true if it was already true or if the gene2 is part of @@gene_array
+                        # adding gene2 to the network_genes if it wasn't there
+                        network_genes[gene2] = {in_gene_list?: in_gene_list, interactions: []} if network_genes[gene2].nil?
+                        # adding gene1 as an interaction of gene2 if it wasn't already there
+                        network_genes[gene2][:interactions] |= gene1
+                        # adding gene2 as an interaction of gene1 if it wasn't already there
+                        network_genes[gene1][:interactions] |= gene2
+
+                        # problem! now if gene1-gene2 and gene2-gene3-gene4, how do I keep track of this
+
+                    else
+
+                    end
+                end
+            end
+            
+            # if there is more than one gene from @@gene_array, create the InteractionNetwork object
+        end
+    end =end
 
 
 
---
+++ Assignment2/README.md
@@ -0,0 +1,11 @@
+This code requires the gem rgl, version 0.5.7
+(https://github.com/monora/rgl)
+I have installed it using:
+sudo gem install rgl
+
+Command to run the code:
+
+
+Command to run the tests:
+ruby AnnotationTest.rb
+
