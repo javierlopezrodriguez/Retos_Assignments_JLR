231cb9f57487c59b3cfa6cff11511f5596fd9e8f
Javier Lopez Rodriguez <javierlopezbq@gmail.com>
2021-11-06 17:33:03 -0400

    more things

--- Assignment3/assignment3.rb
+++ Assignment3/assignment3.rb
@@ -117,8 +117,14 @@ end
 
 def find_seq_in_exons(embl_hash)
     embl_hash.each do |gene_id, bio_embl| # iterating over the hash
-        embl_seq = bio_embl.seq
-        bio_embl.features.each do |feature| # for each feature
+
+        bio_embl_as_biosequence = bio_embl.to_biosequence # turning the Bio::EMBL into Bio::Sequence so that I can add features
+        embl_seq = bio_embl.seq # getting the dna sequence as Bio::Sequence::NA
+
+        # iterating through each feature of the original bio_embl
+        # not using bio_embl_as_biosequence here because I will be adding features 
+        # and it's not good to manipulate an object while iterating through it
+        bio_embl.features.each do |feature| 
             next unless feature.feature == "exon" # skip if the feature is not an exon
             
             ###### vvvv for testing
@@ -131,16 +137,79 @@ def find_seq_in_exons(embl_hash)
 
             next unless feature.assoc["note"].match(Regexp.new(gene_id.to_s, "i")) # skip if the exon is from another gene (because of overlapping or whatever)
 
-            exon_seq = embl_seq.subseq(feature.location.from, feature.location.to)
+            bio_location = feature.locations[0] # feature.locations is Bio::Locations, which is basically an array of Bio::Location objects
+            exon_seq = embl_seq.subseq(bio_location.from, bio_location.to)
 
-            # using lookahead (?=) so that I can match overlapping expressions (https://stackoverflow.com/questions/41028602/find-overlapping-regexp-matches)
-            regexp_f = Regexp.new(/(?=(CTTCTT))/i) 
-            regexp_r = Regexp.new(/(?=(AAGAAG))/i)
+            
+            ########################################
+            ######## This comment is for myself to understand this:
 
-            match_f = exon_seq.match(regexp_f)
-            match_r = exon_seq.match(regexp_r)
+            ##The sequence of the embl is always 5'3' and the exon can be on that one or on the complement one
+            ######## https://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id=AT2G46340&style=raw
+            ## that (embl) has the sequence 5'3', the exons are on the complement, so we would need the reverse complement of that
+            ## if we translate this sequence as is, we dont get the proteins, bc the exons are on the complement
+            ## doing: get_embl([:AT2G46340])[:AT2G46340].seq gives me the above sequence
+            ## if I want to search for CTTCTT on the exons, I need to reverse complement the sequence or the query
+            ## because the exons are on the complement strand
 
-            unless match_f || match_r # if there is no match
+            ######## https://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=fasta&id=AT2G46340&style=raw
+            ## that (fasta) has the reverse complement already -> from here if we translate this we get the proteins
+            ## here we would always look for CTTCTT because it is the correct strand (if we didn't want exons only)
+            ########################################
+
+            if bio_location.strand == +1 # the feature is on the current strand, we need to search for CTTCTT
+                # To find the sequence, I'm using lookahead (?=) so that I can match overlapping expressions 
+                # (https://stackoverflow.com/questions/41028602/find-overlapping-regexp-matches)
+                # (https://stackoverflow.com/questions/5241653/ruby-regex-match-and-get-positions-of)
+                start_f = my_seq.enum_for(:scan, /(?=(cttctt))/i).map { Regexp.last_match.begin(0) + 1} # 1-indexed
+                
+                if start_f.empty? # if there is no match
+
+                    ###### Testing
+                    puts "No match!"
+                    puts gene_id
+                    puts exon_seq
+                    ###### Testing
+    
+                    next # next feature
+                end
+
+                # if there is a match
+                positions_f = start_f.map {|pos| [pos, pos + 5]} unless start_f.empty? # 1-indexed
+
+                ####### create feature en la misma strand
+
+            elsif bio_location.strand == -1 # the feature is on the complement strand, we need to search for the reverse complement, AAGAAG
+                # To find the sequence, I'm using lookahead (?=) so that I can match overlapping expressions 
+                # (https://stackoverflow.com/questions/41028602/find-overlapping-regexp-matches)
+                # (https://stackoverflow.com/questions/5241653/ruby-regex-match-and-get-positions-of)
+                start_r = my_seq.enum_for(:scan, /(?=(aagaag))/i).map { Regexp.last_match.begin(0) + 1} # 1-indexed
+
+                if start_r.empty? # if there is no match
+
+                    ###### Testing
+                    puts "No match!"
+                    puts gene_id
+                    puts exon_seq
+                    ###### Testing
+    
+                    next # next feature
+                end
+
+                # if there is a match
+                positions_r = start_r.map {|pos| [pos, pos + 5]} unless start_r.empty? # 1-indexed
+                # create feature en la otra strand
+
+
+            else
+                puts "WARNING: no strand"
+                next # skip
+            end
+
+            
+            
+
+            if start_f.empty? && start_r.empty? # if there is no match
 
                 ###### Testing
                 puts "No match!"
@@ -148,14 +217,16 @@ def find_seq_in_exons(embl_hash)
                 puts exon_seq
                 ###### Testing
 
-                next
+                next # next feature
             end
             # if there is at least one match
-            if match_f
+            unless start_f.empty?
+                
 
             end
 
-            if match_r
+            unless start_r.empty?
+                
 
             end
         end
@@ -172,16 +243,53 @@ end
 # there are methods that will tell you the starting and ending coordinates of the match in the string)
 ############
 
+def create_feature(position, gene_id)
+    return Bio::Feature.new(feature = 'repeat_sequence', 
+                            position = place..holder, 
+                            qualifiers = [Bio::Feature::Qualifier.new('gene', placeholder),
+                                            Bio::Feature::Qualifier.new('sequence', 'CTTCTT'),
+                                            Bio::Feature::Qualifier.new('strand', placeholder),
+                                            Bio::Feature::Qualifier.new('start', placeholder),
+                                            Bio::Feature::Qualifier.new('end', placeholder)])
+end
+
 ############
 # Task 4a:
 # Once you have found them all, and added them all, loop over each one of your CTTCTT features 
 # (using the #features method of the EnsEMBL Sequence object) and create a GFF3-formatted file of these features.
 ############
 
+def write_gff3_local(parameters)
+    write_gff3(mode = "local", parameters)
+end
+
+def write_gff3_global(parameters)
+    write_gff3(mode = "global", parameters)
+end
+
+def write_gff3(mode, parameters)
+
+    #seqid # id of the landmark: gene id in local, chr number in global
+    #source = "BioRuby"
+    #type = "direct_repeat" # SO:0000314, direct repeat because in CTTCTT, CTT is repeated in the same orientation
+    #start_pos # relative to the gene id in local, relative to the chr in global
+    #end_pos # same as above
+    #score = "."
+    #strand # relative to the landmark
+    #phase = "." # not a CDS
+    #attributes # ID=unique_id;Name=CTTCTT repeat;
+
+
+end
+
 ############
 # Task 4b:
 # Output a report showing which (if any) genes on your list do NOT have exons with the CTTCTT repeat
 ############
+
+def write_report
+
+end
 
 ############
 # Task 5:
